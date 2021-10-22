a2155b15f8b087793b4e7d7e9f3874c0662f769e
EdwardF48 <edwardfurlong48@gmail.com>
2021-10-22 12:58:19 +0100

    006: Item cleanup

--- src/main/java/dto/Item.java
+++ src/main/java/dto/Item.java
@@ -20,24 +20,12 @@ public class Item {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getCost() {
         return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
     }
 
     public int getStockLeft() {
